255c46c11fd8c97b25709ba99cc489112826d644
Gordon Rogers <54502028+gordonrogers123@users.noreply.github.com>
2020-11-15 13:20:58 -0600

    Add files via upload

--- HW5.js
+++ HW5.js
@@ -13,36 +13,39 @@ app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 app.set("port", 8591);
 
-var paramValPair = function (object) {
-  var keys = [];
-  for (var q in object) {
-    keys += "<td>" + q + "</td>" + "<td>" + object[q] + "</td>";
-  }
-  return "<tr>" + keys + "</tr>";
-};
 
+//MUST CITE!
 app.get("/", function (req, res) {
   var getResponse = "<h1>GET Request Received</h1><br><br>";
-  var table = "<table><tr><td>url: " + req.url + "</td>";
-  table += "<td>body: " + paramValPair(req.body) + "</td>";
-  table += "</tr></table>";
-  res.send(getResponse + table);
+  var qParams = [];
+  for (var p in req.query){
+    qParams.push({'name':p,'value':req.query[p]})
+  }
+  var context = {};
+  context.dataList = qParams;
+  res.render(getResponse + 'get-loopback-improved', context);
 });
 
+//MUST CITE!
 app.post("/", function (req, res) {
   var postResponse = "<h1>POST Request Received</h1><br><br>";
-  var table = "<table><tr>";
-  table += "<td>names: " + req.url + "</td>";
-  table += "<td>values: " + paramValPair(req.body) + "</td>";
-  table += "</tr></table>";
-  res.send(postResponse + table);
+  var qParams = [];
+  for (var p in req.query){
+    qParams.push({'name':p,'value':req.query[p]})
+  }
+  var context = {};
+  context.dataList = qParams;
+  res.send(postResponse + 'POST_response', context);
 });
 
+
+//404-error
 app.use(function (req, res) {
   res.status(404);
   res.render("404");
 });
 
+//500-error
 app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.type("plain/text");
@@ -50,6 +53,7 @@ app.use(function (err, req, res, next) {
   res.render("500");
 });
 
+//listener for selected port
 app.listen(app.get("port"), function () {
   console.log(
     "Express started on http://flip3.engr.oregonstate.edu/:" +
